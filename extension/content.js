864dd48ee743b47732d1847a112d515373bb3376
Aditya <adityasjain21@gmail.com>
2025-03-27 01:01:59 +0530

    Modified content.js as per new model

--- extension/content.js
+++ extension/content.js
@@ -1,36 +1,100 @@
-function extractFeatures() {
+const { URL } = require('url');
+
+function havingIP(url) {
+    return /\b(?:\d{1,3}\.){3}\d{1,3}\b/.test(url) ? 1 : -1;
+}
+
+function haveAtSign(url) {
+    return url.includes('@') ? 1 : -1;
+}
+
+function getLength(url) {
+    return url.length < 54 ? -1 : url <= 75 ? 0 : 1;
+}
+
+function getDepth(url) {
+    return (new URL(url).pathname.match(/\//g) || []).length;
+}
+
+function redirection(url) {
+    let pos = url.lastIndexOf('//');
+    return pos > 6 ? (pos > 7 ? 1 : -1) : -1;
+}
+
+function httpDomain(url) {
+    return new URL(url).hostname.includes('https') ? 1 : -1;
+}
+
+const shorteningServices = [
+    "bit.ly", "goo.gl", "shorte.st", "go2l.ink", "x.co", "ow.ly", "t.co", "tinyurl.com",
+    "tr.im", "is.gd", "cli.gs", "yfrog.com", "migre.me", "ff.im", "tiny.cc", "url4.eu"
+];
+
+function tinyURL(url) {
+    return shorteningServices.some(service => url.includes(service)) ? 1 : -1;
+}
+
+function prefixSuffix(url) {
+    return new URL(url).hostname.includes('-') ? 1 : -1;
+}
+
+function getSubDomainLevel(url) {
+    return new URL(url).hostname.split('.').length - 2;
+}
+
+function hasRequestURL(url) {
+    return url.includes('img[src*=\'http\']') ? 1 : -1;
+}
+
+function hasURLAnchor(url) {
+    return url.includes('a[href*=\'http\']') ? 1 : -1;
+}
+
+function hasLinksInTags(url) {
+    return url.includes('link[href*=\'http\']') ? 1 : -1;
+}
+
+function hasSubmittingToEmail(url) {
+    return url.includes('a[href^=mailto]') ? 1 : -1;
+}
+
+function hasMouseoverEffect(url) {
+    return url.includes('a[onmouseover]') ? 1 : -1;
+}
+
+function hasRightClickDisabled(url) {
+    return url.includes('document.oncontextmenu=null') ? -1 : 1;
+}
+
+function hasIframeRedirection(url) {
+    return url.includes('iframe') ? 1 : -1;
+}
+
+function extractURLFeatures(url) {
     return [
-        isIPInURL(),
-        getURLLength(),
-        isShortenedURL(),
-        hasAtSymbol(),
-        hasPrefixSuffix(),
-        getSubDomainLevel(),
-        getSSLState(),
-        getDomainRegistrationLength(),
-        hasHTTPSInURL(),
-        hasRequestURL(),
-        hasURLAnchor(),
-        hasLinksInTags(),
-        hasSFH(),
-        hasSubmittingToEmail(),
-        isAbnormalURL(),
-        hasMouseoverEffect(),
-        hasRightClickDisabled(),
-        hasPopupWindow(),
-        hasIframeRedirection(),
-        getAgeOfDomain(),
-        hasDNSRecord(),
-        getWebTraffic(),
-        isGoogleIndexed(),
-        hasLinksPointingToPage(),
-        hasStatisticalReport()
+        havingIP(url),
+        haveAtSign(url),
+        getLength(url),
+        getDepth(url),
+        redirection(url),
+        httpDomain(url),
+        tinyURL(url),
+        prefixSuffix(url),
+        getSubDomainLevel(url),
+        hasRequestURL(url),
+        hasURLAnchor(url),
+        hasLinksInTags(url),
+        hasSubmittingToEmail(url),
+        hasMouseoverEffect(url),
+        hasRightClickDisabled(url),
+        hasIframeRedirection(url)
     ];
 }
 
 // Extract features and predict phishing status
 async function checkPhishing() {
-    const features = extractFeatures();
+    const url = window.location.href;
+    const features = extractURLFeatures(url);
     console.log("Extracted Features:", features);
 
     // Send features to background script for prediction
@@ -53,105 +117,6 @@ async function checkPhishing() {
 // Run when page loads
 window.addEventListener("load", checkPhishing);
 
-// Feature Extraction Functions
-function isIPInURL() {
-    var reg = /\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3}/;
-    return reg.test(window.location.href) ? 1 : -1;
-}
-
-function getURLLength() {
-    var url = window.location.href.length;
-    return url < 54 ? -1 : url <= 75 ? 0 : 1;
-}
-
-function isShortenedURL() {
-    return window.location.href.length > 20 ? -1 : 1;
-}
-
-function hasAtSymbol() {
-    return window.location.href.includes("@") ? 1 : -1;
-}
-
-function hasPrefixSuffix() {
-    return window.location.href.includes("-") ? 1 : -1;
-}
-
-function getSubDomainLevel() {
-    return window.location.hostname.split(".").length - 2;
-}
-
-function getSSLState() {
-    return window.location.protocol === "https:" ? -1 : 1;
-}
-
-function getDomainRegistrationLength() {
-    return document.location.hostname.length > 1 ? 1 : -1;
-}
-
-function hasHTTPSInURL() {
-    return window.location.href.includes("https") ? 1 : -1;
-}
-
-function hasRequestURL() {
-    return document.querySelectorAll("img[src*='http']").length > 0 ? 1 : -1;
-}
-
-function hasURLAnchor() {
-    return document.querySelectorAll("a[href*='http']").length > 0 ? 1 : -1;
-}
-
-function hasLinksInTags() {
-    return document.querySelectorAll("link[href*='http']").length > 0 ? 1 : -1;
-}
-
-function hasSFH() {
-    return document.querySelectorAll("form[action='']").length > 0 ? 1 : -1;
-}
-
-function hasSubmittingToEmail() {
-    return document.querySelectorAll("a[href^=mailto]").length > 0 ? 1 : -1;
-}
-
-function isAbnormalURL() {
-    return window.location.hostname.includes("-") ? 1 : -1;
-}
-
-function hasMouseoverEffect() {
-    return document.querySelectorAll("a[onmouseover]").length > 0 ? 1 : -1;
-}
-
-function hasRightClickDisabled() {
-    return document.oncontextmenu === null ? -1 : 1;
-}
-
-function hasPopupWindow() {
-    return document.querySelectorAll("script[popup]").length > 0 ? 1 : -1;
-}
-
-function hasIframeRedirection() {
-    return document.querySelectorAll("iframe").length > 0 ? 1 : -1;
-}
-
-function getAgeOfDomain() {
-    return document.location.hostname.length > 1 ? 1 : -1;
-}
-
-function hasDNSRecord() {
-    return document.location.hostname.length > 1 ? 1 : -1;
-}
-
-function getWebTraffic() {
-    return document.referrer ? 1 : -1;
-}
-
-function isGoogleIndexed() {
-    return window.location.hostname.includes("google") ? 1 : -1;
-}
-
-function hasLinksPointingToPage() {
-    return document.querySelectorAll("a").length > 5 ? 1 : -1;
-}
-
-function hasStatisticalReport() {
-    return document.querySelectorAll("script").length > 5 ? 1 : -1;
-}
+// Example Usage
+const exampleUrl = "http://www.example.com/test/page.html";
+console.log(extractURLFeatures(exampleUrl));
