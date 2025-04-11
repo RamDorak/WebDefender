c02b17c0adee1023ca863e4e44abf78c3193cba8
RamDorak <ramdorak@gmail.com>
2025-04-11 14:56:54 +0530

    Updated Code

--- extension/background.js
+++ extension/background.js
@@ -1,8 +1,25 @@
-chrome.extension.onRequest.addListener(function(prediction){
-    if (prediction == 1){
-        alert("Warning: Phishing detected!!");
+console.log('Service worker registered.');
+
+chrome.runtime.onMessage.addListener(function(request) {
+    const prediction = request.prediction;
+    console.log(prediction);
+  
+    if (prediction === 1) {
+      chrome.notifications.create({
+        type: 'basic',
+        iconUrl: 'icon.png',
+        title: 'WebDefender Warning!',
+        message: 'Phishing detected on this page!',
+        priority: 2
+      });
+    } else if (prediction === -1) {
+      chrome.notifications.create({
+        type: 'basic',
+        iconUrl: 'icon.png',
+        title: 'WebDefender Info',
+        message: 'No phishing detected on this page.',
+        priority: 0
+      });
     }
-    else if (prediction == -1){
-        alert("No phishing detected");
-    }
+  
 });
